362bd24da2776221865e885a2e1f3dbc89b85b18
MoumitaRashidBushra <moumitarashidbushra@gmail.com>
2023-05-18 11:47:00 +0600

    .env.local file setup

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAIdEu6DyL8BHdGn6rRHULQwbO7B8b_GG8",
-    authDomain: "toy-zone-c7145.firebaseapp.com",
-    projectId: "toy-zone-c7145",
-    storageBucket: "toy-zone-c7145.appspot.com",
-    messagingSenderId: "484150688445",
-    appId: "1:484150688445:web:4afc018751c95b8861d5d4"
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID
 };
 
 const app = initializeApp(firebaseConfig);
